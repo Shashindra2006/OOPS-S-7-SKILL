9d1887125306fa4f57120f259668de2081565a98
Shashindra2006 <2420030360@klh.edu.in>
2025-07-30 11:20:49 +0530

    all string functions

---
+++ src/string/TrimWhitespace.java
@@ -0,0 +1,9 @@
+package string;
+
+public class TrimWhitespace {
+	public static void main(String[] args) {
+        String s = "  Hello  ";
+        System.out.println("[" + s.trim() + "]");     
+    }
+
+}
